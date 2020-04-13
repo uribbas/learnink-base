f009b56017ca28cf35f242a7b9ddbd44a92109a9
Suchibrata <mailto.dutta.joy@gmail.com>
2020-04-13 08:03:15 +0530

    Fixing bug of automatic OTP verification Adding model based transaction template for interacting with firestore

---
+++ lib/src/models/job.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/foundation.dart';
+
+class Job{
+  Job({@required this.id, @required this.name, @required this.ratePerHour});
+  final String name;
+  final String id;
+  final int ratePerHour;
+
+  factory Job.fromMap(Map<String,dynamic> data,String documentId){
+    if(data==null){
+      return null;
+    }
+    final String name=data['name'];
+    final int ratePerHour=data['ratePerHour'];
+
+    return Job(name:name,ratePerHour: ratePerHour,id:documentId);
+  }
+
+  Map<String,dynamic> toMap(){
+    return {
+      'name':name,
+      'ratePerHour':ratePerHour,
+    };
+  }
+}
---
+++ lib/src/services/database.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+import 'package:meta/meta.dart';
+import '../models/job.dart';
+import 'api_path.dart';
+import 'firestore_service.dart';
+
+abstract class Database {
+  Future<void> setJob(Job job);
+  Stream<List<Job>> jobsStream();
+}
+
+String documentIdFromCurrentDate() => DateTime.now().toIso8601String();
+
+class FirestoreDatabase implements Database {
+  FirestoreDatabase({@required this.uid}) : assert(uid != null);
+  final String uid;
+
+  final _service = FirestoreService.instance;
+
+  Future<void> setJob(Job job) async => await _service.setData(
+        path: APIPath.job(uid, job.id),
+        data: job.toMap(),
+      );
+
+  Stream<List<Job>> jobsStream() => _service.collectionStream(
+        path: APIPath.jobs(uid),
+        builder: (data,documentId) => Job.fromMap(data,documentId),
+      );
+}
